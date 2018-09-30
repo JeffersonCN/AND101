2adb3e40e2a70ca8070529c27d3b7005fc1c677f
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-09-30 08:51:34 -0300

    Alter the MainActivity to calculate de Tip

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -1,47 +1,49 @@
 ﻿using System;
+using System.Globalization;
 using Android.App;
 using Android.OS;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using String = Java.Lang.String;
 
 namespace TipCalculator
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private EditText _amountInput;
+        private Button _calculateBtn;
+        private TextView _tipText;
+        private TextView _totalText;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState) 
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
 
-            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            SetContentView(Resource.Layout.tip_calc_activity);
 
-            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            _amountInput = FindViewById<EditText>(Resource.Id.amountInput);
+            _calculateBtn = FindViewById<Button>(Resource.Id.calculateButton);
+            _tipText = FindViewById<TextView>(Resource.Id.tipValueText);
+            _totalText = FindViewById<TextView>(Resource.Id.totalValueText);
+
+            _calculateBtn.Click += OnCalculateBtnClick;
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        private void OnCalculateBtnClick(object sender, EventArgs e)
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+            if (!decimal.TryParse(_amountInput.Text, NumberStyles.AllowDecimalPoint, new NumberFormatInfo(), out var amount))
+                return;
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            var id = item.ItemId;
+            var tip = amount * 0.15m;
+            var total = amount + tip;
 
-            return id == Resource.Id.action_settings || base.OnOptionsItemSelected(item);
+            _tipText.Text = $"{tip:C}";
+            _totalText.Text = $"{total:C}";
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            var view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-        }
-	}
+    }
 }
 
